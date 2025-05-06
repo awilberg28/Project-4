b181e2f9433a8bf08da1868a3659b4644dc8e257
Morgan Powers <morganpowers@Jessicas-MacBook-Air-3.local>
2025-05-05 21:15:11 -0500

    images displayed

--- DenoisingAutoencoder/visualization.py
+++ DenoisingAutoencoder/visualization.py
@@ -5,14 +5,36 @@ from matplotlib import pyplot as plt
 from keras.datasets import mnist
 from keras.preprocessing.image import load_img, img_to_array
 
+training_set, testing_set = mnist.load_data()
+
+def displayListOfImgs(images):
+    num_images = len(images)
+    num_cols = round(num_images/2)
+    num_rows = (num_images + num_cols - 1) // num_cols
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+
+    if num_rows > 1 or num_cols > 1:axes = axes.flatten()
+    else:axes = [axes]
+
+    for (i,image) in enumerate(firstTen):
+        if i < len(axes):
+            axes[i].imshow(image)
+            axes[i].set_title(f"Image {i+1}")
+            axes[i].axis('off')
+    
+    if num_images < len(axes):
+        for i in range(num_images, len(axes)):fig.delaxes(axes[i])
+
+    plt.tight_layout()
+    plt.show()
 
 def display_MNIST_samples():
-    # ** YOUR CODE HERE **
-    
+    displayListOfImgs(training_set[0][:10])
+    displayListOfImgs(testing_set[0][:10])
 
 def display_NoisyOffice_samples():
-    # ** YOUR CODE HERE **
-    
+    return
 
 display_MNIST_samples()
 display_NoisyOffice_samples()
