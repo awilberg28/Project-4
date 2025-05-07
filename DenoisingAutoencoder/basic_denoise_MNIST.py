80222b53d6f122852205657f5fbbe8f036e5abcf
Addison Wilberg <addisonwilberg@mac.trinity.local>
2025-05-06 20:24:47 -0500

    Basic Denoiser, Needs visualization redone

--- DenoisingAutoencoder/basic_denoise_MNIST.py
+++ DenoisingAutoencoder/basic_denoise_MNIST.py
@@ -6,5 +6,51 @@ from keras.layers import Dense
 from matplotlib import pyplot as plt
 import numpy as np
 import random
+import visualization
+from keras.layers import Activation
+
 
  # ** YOUR CODE HERE **
+
+
+(train, trainLabel), (test, testLabel) = mnist.load_data()
+train = train.reshape((len(train),28*28))
+test = test.reshape((len(test),28*28))
+train = train.astype('float32') / 255. 
+test = test.astype('float32') / 255. 
+
+noise_factor = 0.5
+train_noisy = train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=train.shape)
+test_noisy = test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=test.shape)  
+
+train_noisy = np.clip(train_noisy, 0., 1.)
+test_noisy = np.clip(test_noisy, 0., 1.)
+
+
+model = Sequential()
+model.add(Dense(256, activation='exponential', input_shape=(784,)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(784,activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy')
+
+model.fit(train_noisy, train,
+          epochs=4,
+          batch_size=64,
+          shuffle=True,
+          validation_data=(test_noisy, test))
+
+decoded_imgs = model.predict(test_noisy)
+
+
+images = []
+
+for i in range(5):
+    images.append(test[i].reshape(28,28))
+    images.append(test_noisy[i].reshape(28,28))
+    images.append(decoded_imgs[i].reshape(28,28))
+
+visualization.displayListOfImgs(images)
+
+ 
--- DenoisingAutoencoder/visualization.py
+++ DenoisingAutoencoder/visualization.py
@@ -5,14 +5,64 @@ from matplotlib import pyplot as plt
 from keras.datasets import mnist
 from keras.preprocessing.image import load_img, img_to_array
 
+(training_images, training_labels), (testing_images, testing_labels) = mnist.load_data()
+
+def displayListOfImgs(images):
+    num_images = len(images)
+    num_cols = round(num_images/2)
+    num_rows = (num_images + num_cols - 1) // num_cols
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+
+    if num_rows > 1 or num_cols > 1:axes = axes.flatten()
+    else:axes = [axes]
+
+    for (i,image) in enumerate(images):
+        if i < len(axes):
+            axes[i].imshow(image)
+            axes[i].set_title(f"Image {i+1}")
+            axes[i].axis('off')
+    
+    if num_images < len(axes):
+        for i in range(num_images, len(axes)):fig.delaxes(axes[i])
+
+    plt.tight_layout()
+    plt.show()
 
 def display_MNIST_samples():
-    # ** YOUR CODE HERE **
+    samples = []
+    for digit in range(10):
+        # Get the first image that matches the current digit
+        index = np.where(training_labels == digit)[0][0]
+        samples.append(training_images[index])
+
+    displayListOfImgs(samples)
     
 
 def display_NoisyOffice_samples():
-    # ** YOUR CODE HERE **
-    
+    clean_path = './Noisy_Documents/clean/'
+    noisy_path = './Noisy_Documents/noisy/'
 
-display_MNIST_samples()
-display_NoisyOffice_samples()
+    image_pairs = []
+    for i in range(3):
+        clean_img_path = os.path.join(clean_path, f"{i}.png")
+        noisy_img_path = os.path.join(noisy_path, f"{i}.png")
+
+      
+        clean_img = img_to_array(load_img(clean_img_path))
+        noisy_img = img_to_array(load_img(noisy_img_path))
+
+        image_pairs.append((clean_img / 255.0, noisy_img / 255.0)) 
+
+    fig, axes = plt.subplots(2, 3, figsize=(10, 5))
+    for col in range(3):
+        axes[0, col].imshow(image_pairs[col][0])
+        axes[0, col].set_title(f"Clean {col}.png")
+        axes[0, col].axis('off')
+
+        axes[1, col].imshow(image_pairs[col][1])
+        axes[1, col].set_title(f"Noisy {col}.png")
+        axes[1, col].axis('off')
+
+    plt.tight_layout()
+    plt.show()
